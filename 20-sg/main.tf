b81d0b8928cec7580b8b93afeed958f2d58431fb
Saiteja93 <saitejaraj.guvvala@gmail.com>
2025-04-09 13:05:38 -0700

    sg

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -1,9 +1,9 @@
 module "mysql_sg" {
-    source = "../terraform-aws-security-group"
+    source = "git::https://github.com/Saiteja93/Terraform-aws-security-group"
     project_name = var.project_name
     environment = var.environment
     sg_name = "mysql"
-    vpc_id = module.vpc.vpc_id
+    vpc_id = local.vpc_id
     common_tags = var.common_tags
     sg_tags = var.mysql_sg_tags
 }
